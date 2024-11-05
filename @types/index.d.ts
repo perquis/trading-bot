90ef8da25c5b8537a502c9f7bd998d434e66cb14
perquis <thetahirpl@gmail.com>
2024-11-05 17:57:44 +0100

    :tada: init(app): Initialized node.js boilerplate to create functionality for trading bot.

---
+++ @types/index.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly NODE_ENV?: 'development' | 'production' | 'test';
+      readonly PORT?: string;
+      readonly MONGO_URI?: string;
+      readonly BINANCE_API_KEY?: string;
+      readonly BINANCE_API_SECRET?: string;
+    }
+  }
+}
+
+export {};
